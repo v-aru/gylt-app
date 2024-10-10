5224d9f31e467b231771e46661637dd3edbd460f
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-10-10 16:32:56 +0200

    Add backend api call to db for get, post and put methods

---
+++ src/backend/dbConnect.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+console.log("mongodbURI: ", process.env.MONGODB_URI);
+const dbConnect = async () => {
+  if (mongoose.connection.readyState >= 1) return;
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+  }
+};
+
+export default dbConnect;
---
+++ src/backend/model/Habit.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const habitSchema = new Schema({
+  habitName: { type: String, required: true },
+  frequency: { type: String, required: true },
+  category: { type: String, required: true },
+  days: { type: [String], default: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] },
+  date: { type: Date, default: Date.now },
+  color: { type: String, default: '#000000' },
+  completed: { type: Boolean, default: false },
+});
+
+const Habit = mongoose.models.Habit || mongoose.model('Habit', habitSchema, 'habits-collection');
+
+export default Habit; 
