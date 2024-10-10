2021210a286e47e894a79e1acabb4ba42f6fa72b
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-10-10 14:23:48 +0200

    Fix path resolution

--- pages/api/habits/[id].js
+++ pages/api/habits/[id].js
@@ -1,6 +1,6 @@
 import dbConnect from '@/backend/dbConnect';
 // import Habit from '@/backend/models/Habit'; 
-import Habit from '@backend/models/Habit';
+import Habit from '@/backend/models/Habit';
 
 export default async function handler(req, res) {
   await dbConnect();
