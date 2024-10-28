8edfb7e1a8cf2973050598493f5b7f2917e442b0
Arunima Venkatesan <venkatesan.arunima@gmail.com>
2024-10-29 00:00:45 +0530

    Fix build issue for dashboard page

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { DashboardContainer, SectionCard, SectionTitle } from './DashboardStyles';
 import DashboardHabitList from './DashboardHabitList';
 import DashboardToDoList from './DashboardToDoList';
 import DashboardSubscriptionList from './DashboardSubscriptionList';
+import { HabitsContext } from '@/context/HabitsContext';
 
 const Dashboard = () => {
+    const habitsContext = useContext(HabitsContext);
+
+    if (!habitsContext) {
+        return null; // or return a loading indicator
+    }
+
+    const { habits } = habitsContext;
+
     return (
         <DashboardContainer>
             <SectionCard>
@@ -13,7 +22,7 @@ const Dashboard = () => {
             </SectionCard>
             <SectionCard>
                 <SectionTitle>Habits</SectionTitle>
-                <DashboardHabitList />
+                <DashboardHabitList habits={habits}/>
             </SectionCard>
             <SectionCard>
                 <SectionTitle>Subscriptions</SectionTitle>
